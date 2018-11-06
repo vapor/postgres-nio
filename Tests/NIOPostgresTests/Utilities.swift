8d37aeed2ba661327a1a913f87410a0dbb294496
Tanner Nelson <me@tanner.xyz>
2018-11-06 17:27:48 -0500

    use different ip on linux

--- Tests/NIOPostgresTests/Utilities.swift
+++ Tests/NIOPostgresTests/Utilities.swift
@@ -5,7 +5,13 @@ extension PostgresConnection {
     static func test() -> EventLoopFuture<PostgresConnection> {
         let eventLoop = MultiThreadedEventLoopGroup(numberOfThreads: 1).next()
         do {
-            return try connect(to: .init(ipAddress: "127.0.0.1", port: 5432), on: eventLoop).then { conn in
+            let hostname: String
+            #if os(Linux)
+            hostname = "127.0.0.1"
+            #else
+            hostname = "psql"
+            #endif
+            return try connect(to: .init(ipAddress: hostname, port: 5432), on: eventLoop).then { conn in
                 return conn.authenticate(username: "vapor_username", database: "vapor_database", password: "vapor_password")
                     .map { conn }
             }
