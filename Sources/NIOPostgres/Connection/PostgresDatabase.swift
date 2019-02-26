e0b334121ed19f3816586617239727f6109e5718
Tanner Nelson <me@tanner.xyz>
2019-02-25 20:31:20 -0500

    add psql database note

--- Sources/NIOPostgres/Connection/PostgresDatabase.swift
+++ Sources/NIOPostgres/Connection/PostgresDatabase.swift
@@ -1,3 +1,4 @@
+#warning("TODO: make this protocol more general, not depend on PostgresRequestHandler")
 public protocol PostgresDatabase {
     var eventLoop: EventLoop { get }
     func send(_ request: PostgresRequestHandler) -> EventLoopFuture<Void>
