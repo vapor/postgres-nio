192dc5c6b985b300759518f68f95e6260dbe5136
Tanner Nelson <me@tanner.xyz>
2019-05-21 11:52:51 -0400

    fix already closed error

--- Sources/NIOPostgres/Connection/PostgresConnection.swift
+++ Sources/NIOPostgres/Connection/PostgresConnection.swift
@@ -13,22 +13,34 @@ public final class PostgresConnection {
     }
     
     public var logger: Logger
+
+    private var didClose: Bool
     
     init(channel: Channel, logger: Logger) {
         self.channel = channel
         self.logger = logger
+        self.didClose = false
     }
     
     public func close() -> EventLoopFuture<Void> {
-        guard self.channel.isActive else {
+        guard !self.didClose else {
             return self.eventLoop.makeSucceededFuture(())
         }
-        return self.channel.close()
+        self.didClose = true
+        
+        let promise = self.eventLoop.makePromise(of: Void.self)
+        self.eventLoop.submit {
+            switch self.channel.isActive {
+            case true:
+                promise.succeed(())
+            case false:
+                self.channel.close(mode: .all, promise: promise)
+            }
+        }.cascadeFailure(to: promise)
+        return promise.futureResult
     }
     
     deinit {
-        if self.channel.isActive {
-            assertionFailure("PostgresConnection deinitialized before being closed.")
-        }
+        assert(self.didClose, "PostgresConnection deinitialized before being closed.")
     }
 }
