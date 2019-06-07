b306e469ee7d5dda672e1fee633c1c6c44c65086
Tanner Nelson <me@tanner.xyz>
2019-06-07 15:27:55 -0400

    add Optional PostgresDataConvertible conformance

---
+++ Sources/PostgresNIO/Data/PostgresData+Optional.swift
@@ -0,0 +1,18 @@
+extension Optional: PostgresDataConvertible where Wrapped: PostgresDataConvertible {
+    public static var postgresDataType: PostgresDataType {
+        Wrapped.postgresDataType
+    }
+
+    public init?(postgresData: PostgresData) {
+        self = Wrapped.init(postgresData: postgresData)
+    }
+
+    public var postgresData: PostgresData? {
+        switch self {
+        case .some(let wrapped):
+            return wrapped.postgresData
+        case .none:
+            return PostgresData.null
+        }
+    }
+}
