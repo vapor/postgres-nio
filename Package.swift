51cd33c65a532281f59f7cb329d799e25c81ff07
tanner0101 <me@tanner.xyz>
2019-11-18 13:35:03 -0500

    fix deps

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-metrics.git", from: "1.0.0-a"),
+        .package(url: "https://github.com/apple/swift-metrics.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
