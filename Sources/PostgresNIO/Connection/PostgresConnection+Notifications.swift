60d6afcf2a4872775a03dc16a64dcfa4d7f2ef53
Alex Kirchhoff <alex@xnor.ai>
2019-12-11 12:05:05 -0800

    Explicit "self."

--- Sources/PostgresNIO/Connection/PostgresConnection+Notifications.swift
+++ Sources/PostgresNIO/Connection/PostgresConnection+Notifications.swift
@@ -13,7 +13,7 @@ extension PostgresConnection {
     @discardableResult
     public func listen(channel: String, handler notificationHandler: @escaping (PostgresListenContext, PostgresMessage.NotificationResponse) -> Void) -> PostgresListenContext {
         let listenContext = PostgresListenContext()
-        let channelHandler = PostgresNotificationHandler(channel: channel, notificationHandler: notificationHandler, listenContext: listenContext)
+        let channelHandler = PostgresNotificationHandler(channel: channel, notificationHandler: self.notificationHandler, listenContext: listenContext)
         let pipeline = self.channel.pipeline
         _ = pipeline.addHandler(channelHandler, name: nil, position: .last)
         listenContext.stopper = { [pipeline, unowned channelHandler] in
@@ -38,18 +38,18 @@ final class PostgresNotificationHandler: ChannelInboundHandler, RemovableChannel
     }
 
     func channelRead(context: ChannelHandlerContext, data: NIOAny) {
-        let request = unwrapInboundIn(data)
+        let request = self.unwrapInboundIn(data)
         // Slightly complicated: We need to dispatch downstream _before_ we handle the notification ourselves, because the notification handler could try to stop the listen, which removes ourselves from the pipeline and makes fireChannelRead not work any more.
-        context.fireChannelRead(wrapInboundOut(request))
+        context.fireChannelRead(self.wrapInboundOut(request))
         if request.identifier == .notificationResponse {
             do {
                 var data = request.data
                 let notification = try PostgresMessage.NotificationResponse.parse(from: &data)
                 if notification.channel == channel {
-                    notificationHandler(listenContext, notification)
+                    self.notificationHandler(self.listenContext, notification)
                 }
             } catch let error {
-                errorCaught(context: context, error: error)
+                self.errorCaught(context: context, error: error)
             }
         }
     }
