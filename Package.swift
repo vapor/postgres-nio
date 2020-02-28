86b61c0b17d99fac37ac1965382fbd25cceb8786
tanner0101 <me@tanner.xyz>
2020-02-28 13:55:36 -0500

    rc.1

--- Package.swift
+++ Package.swift
@@ -1,27 +1,32 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "postgres-nio",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "PostgresNIO", targets: ["PostgresNIO"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "CMD5", dependencies: []),
         .target(name: "PostgresNIO", dependencies: [
-            "CMD5", "Logging", "Metrics", "NIO", "NIOSSL"
+            .product(name: "Crypto", package: "swift-crypto"),
+            .product(name: "Logging", package: "swift-log"),
+            .product(name: "Metrics", package: "swift-metrics"),
+            .product(name: "NIO", package: "swift-nio"),
+            .product(name: "NIOSSL", package: "swift-nio-ssl"),
         ]),
         .testTarget(name: "PostgresNIOTests", dependencies: [
-            "PostgresNIO", "NIOTestUtils"
+            .target(name: "PostgresNIO"),
+            .product(name: "NIOTestUtils", package: "swift-nio"),
         ]),
     ]
 )
