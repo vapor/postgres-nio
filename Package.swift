2c51dfaef2b6fa1d8a31c1293ca459022d1fd848
Fabian Fett <fabianfett@apple.com>
2021-05-03 16:43:34 +0200

    Add platform requirements for iOS, watchOS and tvOS
    To use postgres-nio from iOS, watchOS and tvOS the platform requirements need to be set correctly.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 let package = Package(
     name: "postgres-nio",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
     ],
     products: [
         .library(name: "PostgresNIO", targets: ["PostgresNIO"]),
