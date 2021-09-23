c048323ee847f4b32756c87045d07a1b7357a0e3
Fabian Fett <fabianfett@apple.com>
2021-09-23 09:31:09 +0200

    Depend on Swift Crypto `"1.0.0" ..< "3.0.0"`
    https://github.com/apple/swift-crypto#compatibility

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.32.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.1"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
     ],
