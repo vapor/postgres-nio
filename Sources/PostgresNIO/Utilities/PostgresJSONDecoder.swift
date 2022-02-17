0b5c40077d5053f4ccf5ba9f23902e4ac9133f7b
Fabian Fett <fabianfett@apple.com>
2022-02-17 19:53:17 +0100

    Remove PSQLJSONDecoder (#216)

--- Sources/PostgresNIO/Utilities/PostgresJSONDecoder.swift
+++ Sources/PostgresNIO/Utilities/PostgresJSONDecoder.swift
@@ -1,10 +1,22 @@
-import Foundation
+import class Foundation.JSONDecoder
+import struct Foundation.Data
+import NIOFoundationCompat
 
 /// A protocol that mimicks the Foundation `JSONDecoder.decode(_:from:)` function.
 /// Conform a non-Foundation JSON decoder to this protocol if you want PostgresNIO to be
 /// able to use it when decoding JSON & JSONB values (see `PostgresNIO._defaultJSONDecoder`)
 public protocol PostgresJSONDecoder {
     func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : Decodable
+
+    func decode<T: Decodable>(_ type: T.Type, from buffer: ByteBuffer) throws -> T
+}
+
+extension PostgresJSONDecoder {
+    public func decode<T: Decodable>(_ type: T.Type, from buffer: ByteBuffer) throws -> T {
+        var copy = buffer
+        let data = copy.readData(length: buffer.readableBytes)!
+        return try self.decode(type, from: data)
+    }
 }
 
 extension JSONDecoder: PostgresJSONDecoder {}
