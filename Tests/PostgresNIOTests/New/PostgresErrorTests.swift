d3789e57259e8cac93cbf26b63b8c323b108eca6
Gwynne Raskind <gwynne@vapor.codes>
2023-06-03 22:28:27 -0500

    Add tests for descriptions of PostgresBinding, PostgresQuery, and PSQLErrror.

--- Tests/PostgresNIOTests/New/PostgresErrorTests.swift
+++ Tests/PostgresNIOTests/New/PostgresErrorTests.swift
@@ -2,6 +2,47 @@
 import XCTest
 import NIOCore
 
+final class PSQLErrorTests: XCTestCase {
+    func testPostgresBindingsDescription() {
+        let testBinds1 = PostgresBindings(capacity: 0)
+        var testBinds2 = PostgresBindings(capacity: 1)
+        testBinds2.append(1, context: .default)
+        
+        XCTAssertEqual(String(describing: testBinds1), "[]")
+        XCTAssertEqual(String(reflecting: testBinds1), "PostgresBindings(metadata: [], bytes: 0 [])")
+        XCTAssertEqual(String(describing: testBinds2), "[BIGINT]")
+        XCTAssertEqual(String(reflecting: testBinds2), "PostgresBindings(metadata: [bin(BIGINT)], bytes: 12 [0,0,0,8,0,0,0,0,...])")
+    }
+
+    func testPostgresQueryDescription() {
+        let testBinds1 = PostgresBindings(capacity: 0)
+        var testBinds2 = PostgresBindings(capacity: 1)
+        testBinds2.append(1, context: .default)
+        let testQuery1 = PostgresQuery(unsafeSQL: "TEST QUERY")
+        let testQuery2 = PostgresQuery(unsafeSQL: "TEST QUERY", binds: testBinds1)
+        let testQuery3 = PostgresQuery(unsafeSQL: "TEST QUERY", binds: testBinds2)
+        
+        XCTAssertEqual(String(describing: testQuery1), "TEST QUERY []")
+        XCTAssertEqual(String(reflecting: testQuery1), "PostgresQuery(sql: TEST QUERY, binds: PostgresBindings(metadata: [], bytes: 0 []))")
+        XCTAssertEqual(String(describing: testQuery2), "TEST QUERY []")
+        XCTAssertEqual(String(reflecting: testQuery2), "PostgresQuery(sql: TEST QUERY, binds: PostgresBindings(metadata: [], bytes: 0 []))")
+        XCTAssertEqual(String(describing: testQuery3), "TEST QUERY [BIGINT]")
+        XCTAssertEqual(String(reflecting: testQuery3), "PostgresQuery(sql: TEST QUERY, binds: PostgresBindings(metadata: [bin(BIGINT)], bytes: 12 [0,0,0,8,0,0,0,0,...]))")
+    }
+
+    func testPSQLErrorDescription() {
+        var error1 = PSQLError.server(.init(fields: [.localizedSeverity: "ERROR", .severity: "ERROR", .sqlState: "00000", .message: "Test message", .detail: "More test message", .hint: "It's a test, that's your hint", .position: "1", .schemaName: "testsch", .tableName: "testtab", .columnName: "testcol", .dataTypeName: "testtyp", .constraintName: "testcon", .file: #fileID, .line: "0", .routine: #function]))
+        var testBinds = PostgresBindings(capacity: 1)
+        testBinds.append(1, context: .default)
+        error1.query = .init(unsafeSQL: "TEST QUERY", binds: testBinds)
+        
+        XCTAssertEqual(String(describing: error1), "Database error")
+        XCTAssertEqual(String(reflecting: error1), """
+            PSQLError(code: server, serverInfo: [sqlState: 00000, detail: More test message, file: PostgresNIOTests/PostgresErrorTests.swift, hint: It's a test, that's your hint, line: 0, message: Test message, position: 1, routine: testPSQLErrorDescription(), localizedSeverity: ERROR, severity: ERROR, columnName: testcol, dataTypeName: testtyp, constraintName: testcon, schemaName: testsch, tableName: testtab], query: PostgresQuery(sql: TEST QUERY, binds: PostgresBindings(metadata: [bin(BIGINT)], bytes: 12 [0,0,0,8,0,0,0,0,...])))
+            """)
+    }
+}
+
 final class PostgresDecodingErrorTests: XCTestCase {
     func testPostgresDecodingErrorEquality() {
         let error1 = PostgresDecodingError(
