097b018dcffc72bae5f688d6a512baa61766df71
Fabian Fett <fabianfett@apple.com>
2023-10-12 16:36:24 +0200

    Add `ConnectionPoolModule`
    We want to land a new ConnectionPool into PostgresNIO in the comming weeks. Since this pool is abstract, let's create a target and product for it. The target and product are both underscored, to signal that we don't make any API stability guarantees.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
@@ -11,9 +11,11 @@ let package = Package(
     ],
     products: [
         .library(name: "PostgresNIO", targets: ["PostgresNIO"]),
+        .library(name: "_ConnectionPoolModule", targets: ["_ConnectionPoolModule"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.4"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.58.0"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.18.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.23.1"),
@@ -25,6 +27,7 @@ let package = Package(
         .target(
             name: "PostgresNIO",
             dependencies: [
+                .target(name: "_ConnectionPoolModule"),
                 .product(name: "Atomics", package: "swift-atomics"),
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "Logging", package: "swift-log"),
@@ -38,12 +41,30 @@ let package = Package(
                 .product(name: "NIOFoundationCompat", package: "swift-nio"),
             ]
         ),
+        .target(
+            name: "_ConnectionPoolModule",
+            dependencies: [
+                .product(name: "Atomics", package: "swift-atomics"),
+                .product(name: "DequeModule", package: "swift-collections"),
+            ],
+            path: "Sources/ConnectionPoolModule"
+        ),
         .testTarget(
             name: "PostgresNIOTests",
             dependencies: [
                 .target(name: "PostgresNIO"),
                 .product(name: "NIOEmbedded", package: "swift-nio"),
                 .product(name: "NIOTestUtils", package: "swift-nio"),
+            ]
+        ),
+        .testTarget(
+            name: "ConnectionPoolModuleTests",
+            dependencies: [
+                .target(name: "_ConnectionPoolModule"),
+                .product(name: "DequeModule", package: "swift-collections"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+                .product(name: "NIOEmbedded", package: "swift-nio"),
             ]
         ),
         .testTarget(
