5b8b65eec6bbf84f1f2f00780bf4838546adc275
Fabian Fett <fabianfett@apple.com>
2023-10-16 12:02:31 +0200

    Fix mac CI

--- Tests/ConnectionPoolModuleTests/NoKeepAliveBehaviorTests.swift
+++ Tests/ConnectionPoolModuleTests/NoKeepAliveBehaviorTests.swift
@@ -1,6 +1,7 @@
 import _ConnectionPoolModule
 import XCTest
 
+@available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
 final class NoKeepAliveBehaviorTests: XCTestCase {
     func testNoKeepAlive() {
         let keepAliveBehavior = NoOpKeepAliveBehavior(connectionType: MockConnection.self)
