212a655233925deeed3248d9be1fe25d1df55e1b
Fabian Fett <fabianfett@apple.com>
2023-10-18 22:06:12 +0200

    Add `PoolStateMachine.ConnectionState`

--- Tests/ConnectionPoolModuleTests/Mocks/MockTimerCancellationToken.swift
+++ Tests/ConnectionPoolModuleTests/Mocks/MockTimerCancellationToken.swift
@@ -2,15 +2,17 @@
 
 @available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
 struct MockTimerCancellationToken: Hashable, Sendable {
-    var connectionID: MockConnection.ID
-    var timerID: Int
-    var duration: Duration
-    var usecase: TestPoolStateMachine.Timer.Usecase
+    enum Backing: Hashable, Sendable {
+        case timer(TestPoolStateMachine.Timer)
+        case connectionTimer(TestPoolStateMachine.ConnectionTimer)
+    }
+    var backing: Backing
 
     init(_ timer: TestPoolStateMachine.Timer) {
-        self.connectionID = timer.connectionID
-        self.timerID = timer.timerID
-        self.duration = timer.duration
-        self.usecase = timer.usecase
+        self.backing = .timer(timer)
+    }
+
+    init(_ timer: TestPoolStateMachine.ConnectionTimer) {
+        self.backing = .connectionTimer(timer)
     }
 }
