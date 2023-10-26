6245bea2e4518aef0056a1c1939aeda5e84e540a
Fabian Fett <fabianfett@apple.com>
2023-10-26 16:31:06 +0200

    Fix CI IV

--- Tests/ConnectionPoolModuleTests/Mocks/MockClock.swift
+++ Tests/ConnectionPoolModuleTests/Mocks/MockClock.swift
@@ -1,6 +1,7 @@
 @testable import _ConnectionPoolModule
 import Atomics
 
+@available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
 final class MockClock: Clock {
     struct Instant: InstantProtocol, Comparable {
         typealias Duration = Swift.Duration
