ea0800d12bbf70a3968b6ccd0cf17bb5d861530f
Timo <38291523+lovetodream@users.noreply.github.com>
2024-01-09 12:53:32 +0100

    Fix Availability for DiscardingTaskGroup on watchOS (#448)

--- Sources/ConnectionPoolModule/ConnectionPool.swift
+++ Sources/ConnectionPoolModule/ConnectionPool.swift
@@ -591,7 +591,7 @@ protocol TaskGroupProtocol {
 }
 
 #if swift(>=5.8) && os(Linux) || swift(>=5.9)
-@available(macOS 14.0, iOS 17.0, tvOS 17.0, watchOS 9.0, *)
+@available(macOS 14.0, iOS 17.0, tvOS 17.0, watchOS 10.0, *)
 extension DiscardingTaskGroup: TaskGroupProtocol {}
 #endif
 
