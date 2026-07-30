5ebcf4f18a273df19b71578ad0d05dba18787987
Alex Hoppen <ahoppen@apple.com>
2026-07-30 10:36:21 +0200

    Support binary data transfer in `COPY FROM` (#573)
    * Support binary data transfer in `COPY FROM`
    
    My benchmark of transferring the integers from 0 to 1,000,000 both as an integer and as a string was about the same speed as the old text-based transfer. I believe that the binary transfer will start to show significant benefits when transferring binary data, other fields that don't need to be represented as fields and also means that the user doesn't need to worry about escapping their data.
    
    * Work around compiler assertion failure
    
    Work around https://github.com/swiftlang/swift/issues/83309
    
    * Non-copyable `ColumnWriter`
    
    * Address review comments
    
    * Update docc comments
    
    ---------
    
    Co-authored-by: Paul Toffoloni <69189821+ptoffy@users.noreply.github.com>
    Co-authored-by: Paul Toffoloni <paultoffoloni@gmail.com>

--- Package.swift
+++ Package.swift
@@ -49,7 +49,7 @@ let package = Package(
                 .product(name: "NIOFoundationCompat", package: "swift-nio"),
                 .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
             ],
-            swiftSettings: swiftSettings
+            swiftSettings: swiftSettings + [.enableExperimentalFeature("Lifetimes")]
         ),
         .target(
             name: "_ConnectionPoolModule",
