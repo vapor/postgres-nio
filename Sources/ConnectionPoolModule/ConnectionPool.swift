3147e5ae386e3855d781bfc144f23f3f3961feb6
Adam Fowler <adamfowler71@gmail.com>
2026-08-03 08:43:29 +0100

    Add missing @available() attributes (#644)
    Co-authored-by: Fabian Fett <fabianfett@apple.com>

--- Sources/ConnectionPoolModule/ConnectionPool.swift
+++ Sources/ConnectionPoolModule/ConnectionPool.swift
@@ -623,6 +623,7 @@ extension DiscardingTaskGroup: TaskGroupProtocol {
     }
 }
 
+@available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
 extension TaskGroup<Void>: TaskGroupProtocol {
     @inlinable
     mutating func addTask_(operation: @isolated(any) @escaping @Sendable () async -> Void) {
