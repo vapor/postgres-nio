bb3fa7a25c2834700b5caec11c100fadbed72c7f
Paul Toffoloni <paultoffoloni@gmail.com>
2026-09-15 10:30:58 +0200

    Update to Swift 6.2

--- Sources/PostgresNIO/New/PostgresRowSequence.swift
+++ Sources/PostgresNIO/New/PostgresRowSequence.swift
@@ -44,7 +44,6 @@ extension PostgresRowSequence {
             self.columns = columns
         }
 
-        #if compiler(>=6.2)
         @concurrent
         public mutating func next() async throws -> Element? {
             if let dataRow = try await self.backing.next() {
@@ -56,18 +55,6 @@ extension PostgresRowSequence {
             }
             return nil
         }
-        #else
-        public mutating func next() async throws -> Element? {
-            if let dataRow = try await self.backing.next() {
-                return PostgresRow(
-                    data: dataRow,
-                    lookupTable: self.lookupTable,
-                    columns: self.columns
-                )
-            }
-            return nil
-        }
-        #endif
 
         @available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
         public mutating func next(isolation actor: isolated (any Actor)?) async throws(Self.Failure) -> PostgresRow? {
